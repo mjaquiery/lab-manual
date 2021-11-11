fa5cb826b48454dd4c3a7cd62e8a33377345329a
marton-balazs-kovacs <43272864+marton-balazs-kovacs@users.noreply.github.com>
2021-11-11 16:09:40 +0100

    added basic sidebar

--- src/main.js
+++ src/main.js
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// import Oruga from '@oruga-ui/oruga-next'
+// import { bulmaConfig } from '@oruga-ui/theme-bulma'
+
+// import '@oruga-ui/theme-bulma/dist/bulma.css'
+// import '@oruga-ui/oruga-next/dist/oruga.css'
+
+createApp(App)
+    // .use(Oruga, bulmaConfig)
+    .use(router)
+    .mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <BurgerButton></BurgerButton>
+    <Sidebar>
+     <ul class="sidebar-panel-nav">
+     </ul>
+   </Sidebar>
     <img alt="Vue logo" src="../assets/logo.png">
     <Item v-for="(I, index) in template.contents" :template="I" :key="index"/>
   </div>
@@ -9,11 +14,15 @@
 // @ is an alias to /src
 import Item from '@/components/Item.vue'
 import template from '@/assets/example.json'
+import Sidebar from '@/components/Sidebar.vue'
+import BurgerButton from '@/components/BurgerButton.vue';
 
 export default {
   name: 'Home',
   components: {
-    Item
+    Item,
+    Sidebar,
+    BurgerButton
   },
   data: function () {
     return {
