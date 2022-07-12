a47b470d67c74bd54c9402e1d0d81fdcd5a8d977
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2022-07-12 22:41:05 +0200

    aligned title and navlinks

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="nav" class="top-0 p-4 w-full h-full flex flex-row">
-    <div class="flex-row flex pr-12">
+  <div id="nav" class="top-0 p-4 w-full flex flex-row">
+    <div class="flex-1 pr-12 justify-start items-center">
       <h1 class="pr-3">Blueprint</h1>
       <h2>Create your own lab manual</h2>
     </div>
-    <div class="flex-row flex">
+    <div class="flex-1 items-center justify-center">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
