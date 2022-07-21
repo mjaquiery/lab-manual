54503f959658ad738115772d81f0f3b0ed853570
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2022-07-21 15:09:40 +0200

    added about page text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="about bg-blue-500 bg-opacity-20 flex flex-auto">
-    <h1>This is an about page</h1>
+    <h1>About this app</h1>
+    <p>
+      Lab manuals are useful tools for documenting the standard functioning of a research laboratory to ensure
+      that all lab members have a shared understanding of who they are and how they do things. The creation of a
+      lab manual should be a joint effort between lab members to ensure that everyone agrees with its content
+      and have their needs taken into consideration.
+    </p>
+    <p>
+      To help researchers create their own lab manual we developed a general lab manual template that lists
+      a wide variety of different topics that could be included in a lab manual in a structured format. For some
+      of the topics where we were expecting little variation in the procedures they cover we include boilerplate
+      texts in the template. Thus, users do not have to reinvent the wheel but they can choose the boilerplate text
+      that is the most suitable for their needs.
+    </p>
+    <p>
+      You can find the current version of the lab manual template here: LINK
+    </p>
+    <p>
+      Using our web application researchers can use the lab manual template as a starting point to create their own
+      manual.
+      The application takes care of the formatting so researhcers can focus on the content of their lab manual. We also
+      wrote a guideline that contains more information on the different topics included in our template and a
+      description
+      of our suggested workflow for creating your own manual.
+    </p>
+    <p>
+      You can find the guideline paper here: LINK
+    </p>
+    <h1>Contributors</h1>
+    <p>Marton Kovacs, Matt Jaquiery, Balazs Aczel</p>
+    <h1>Citation</h1>
+    <p>Coming soon.</p>
+    <h1>Questions and comments</h1>
+    <p>If you have any questions, comments, or requests write to Marton Kovacs at marton.balazs.kovacs at gmail.com or
+      submit an issue on Github.</p>
   </div>
 </template>
